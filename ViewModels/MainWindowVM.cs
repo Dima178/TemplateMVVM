b21b4f6df15887952ba2453fe29164cc5a1aed22
Dima178 <dimagrf@gmail.com>
2023-06-01 18:08:21 +0300

    Добавьте файлы проекта.

---
+++ ViewModels/MainWindowVM.cs
@@ -0,0 +1,25 @@
+﻿using MyTestWPFApp.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTestWPFApp.ViewModels
+{
+    internal class MainWindowVM : ViewModel
+    {
+        //protected override void Dispose(bool Disposing)
+        //{
+        //    base.Dispose(Disposing);
+        //}
+        private string _Title = "Тестовый проект XAML";
+
+        /// <summary>заголовок окна</summary>
+        public string Title 
+        {
+        get => _Title;
+        set =>  Set(ref _Title, value); 
+        }
+    }
+}
