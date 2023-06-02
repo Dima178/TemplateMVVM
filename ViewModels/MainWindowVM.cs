e639219239a808a37e66a07ad993e5577e18ad8b
Dima178 <dimagrf@gmail.com>
2023-06-02 11:45:06 +0300

    Добавление команды выход в главное окно

--- ViewModels/MainWindowVM.cs
+++ ViewModels/MainWindowVM.cs
@@ -1,9 +1,12 @@
-﻿using MyTestWPFApp.ViewModels.Base;
+﻿using MyTestWPFApp.Infrastructure.Commands;
+using MyTestWPFApp.ViewModels.Base;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace MyTestWPFApp.ViewModels
 {
@@ -21,5 +24,29 @@ namespace MyTestWPFApp.ViewModels
         get => _Title;
         set =>  Set(ref _Title, value); 
         }
+        #region Команды
+
+        #region CloseApplicationCommand
+        public ICommand CloseApplicationCommand { get; }
+        private void OnCloseApplicationCommandExecuted(object p) 
+        {
+        Application.Current.Shutdown();
+        }
+        private bool CanCloseApplicationCommandExecute(object p) => true;
+        #endregion
+
+        #endregion
+        public MainWindowVM() 
+        {
+
+            #region Команды
+
+            CloseApplicationCommand = new LambdaCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecute);
+
+
+
+            #endregion
+        }
+
     }
 }
