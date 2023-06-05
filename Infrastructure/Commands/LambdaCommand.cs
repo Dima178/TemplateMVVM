3cecf3e57f305d34f62d6643844ac746a06cfced
Dima178 <dimagrf@gmail.com>
2023-06-05 13:05:11 +0300

    Удаление using

--- Infrastructure/Commands/LambdaCommand.cs
+++ Infrastructure/Commands/LambdaCommand.cs
@@ -1,9 +1,5 @@
 ﻿using MyTestWPFApp.Infrastructure.Commands.Base;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MyTestWPFApp.Infrastructure.Commands
 {
